48a08935cf4b4b607726b4cf7038078615e7509f
agniesme <42878193+agniesme@users.noreply.github.com>
2018-11-23 18:06:08 -0500

    anythingdotcom

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public CharacterController2D controller;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
