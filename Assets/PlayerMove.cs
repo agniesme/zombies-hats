117f04eac8bd33bf7eb66f47effc15dbd4143c7a
and_rei <hiorelse@gmail.com>
2018-11-23 19:06:32 -0800

    move

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
-    public CharacterController2D controller;
 
 	// Use this for initialization
 	void Start () {
-		
+		Move();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
-}
+
+	void FixedUpdate () {
+	}
+
+	public void Move() {
+		transform.Translate(Vector2.right);
+	}
+} 
